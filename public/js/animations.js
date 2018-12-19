a4402d648f54a0c159a3f8c9d925510305d00651
janiswalser <janis@Janiss-MacBook-Pro-2.local>
2018-12-19 21:17:49 +0100

    janis init

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,8 +1,17 @@
 
 var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
+    container: document.getElementById('heartbeat'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'data.json'
+    path: 'json/data.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('heartrate'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'json/heartrate.json'
 })
